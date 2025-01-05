564e089e7c4b95601f5930c38bfd81c8902c5b8b
Hugo R Alves <66341189+r-hugoalves@users.noreply.github.com>
2025-01-05 17:18:57 -0300

    Validate Inputs

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,25 @@ def login():
 def logout():
     return "<h1> Logout </h1>"
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
+    
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        if len(email) <4:
+            flash("E-mail must be greater than 4 characters!", category="error")
+        elif len(firstName) <1:
+            flash("First Name must be greater than 1 character!", category="error")
+        elif password1 != password2:
+            flash("Passwords don't match!", category="error")
+        elif len(password1) < 7:
+            flash("Password must be greater than 7 characters!", category="error")
+        else:
+            # Add User to the Database
+            flash("Account created!", category="success")
+            
     return render_template("signup.html")
